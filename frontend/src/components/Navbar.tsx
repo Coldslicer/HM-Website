51650a27e9e234a1fc25e7e52ab7c7cba162e557
RecoilGaming <qinzhaolee@gmail.com>
2025-02-21 13:24:46 -0800

    fix site favicon not displaying

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from "../assets/LOGONAVBAR.png"
+import logo from "../../assets/logo-navbar.png"
 import { useAuthStore } from '../store/authStore'
 
 export function Navbar() {
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,8 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import "./assets/favicon_io-2/android-chrome-192x192.png"
-import "./assets/favicon_io-2/android-chrome-512x512.png"
-import "./assets/favicon_io-2/apple-touch-icon.png"
-import "./assets/favicon_io-2/favicon-16x16.png"
-import "./assets/favicon_io-2/favicon-32x32.png"
-import "./assets/favicon_io-2/favicon.ico"
-import "./assets/favicon_io-2/site.webmanifest"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
